954949619903fda79997abd32cce357877692aa6
Jakub Strojewski <jakub.strojewski@gmail.com>
2016-02-18 13:51:25 +0100

    Update pipes

--- src/pipes/todos-filter.ts
+++ src/pipes/todos-filter.ts
@@ -5,5 +5,7 @@ import { FilterType, todosFilter } from '../../todo-lib/filters';
 
 @Pipe({name: 'todosFilter'})
 export class TodosFilter implements PipeTransform {
-    transform = todosFilter;
+    transform(todos: ITodo[], [type]: [FilterType]) {
+        return todosFilter(todos, type);
+    };
 }
