574c56c33dcdce7a386c580416fe5dc8e142cd63
Jakub Strojewski <jakub.strojewski@gmail.com>
2016-04-05 15:01:00 +0200

    TUT-2 - Add user

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,21 @@ import { User } from './services/user';
     templateUrl: '/src/app.html'
 })
 export class App {
+
+    newUser: User = null;
+    
     users = [
         new User('Eryk', 'Thorsson', 233),
         new User('Adam', 'Adamov', 53),
         new User('Xena', 'Booby', 25)
     ];
+
+    prepareNewUser() { 
+        this.newUser = new User();
+    }
+    
+    addNewUser(user: User) {
+        this.users = this.users.concat(user);
+        this.newUser = null;
+    }
 }
